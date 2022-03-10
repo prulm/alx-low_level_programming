752cbb45b23bd96d90d7336718df31773924215f
Prulm <jaredyared83@gmail.com>
2022-03-10 19:21:58 +0300

    edited the code for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 int intType;
 float floatType;
@@ -20,6 +20,6 @@ printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-    
-    return 0;
+return 0;
 }
+
