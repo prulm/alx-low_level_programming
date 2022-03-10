b250ab190ad0e86db16dab67f51c18b06b12ccdd
Prulm <jaredyared83@gmail.com>
2022-03-10 19:23:34 +0300

    edited the code for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,6 +20,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-return 0;
-}
+return 0;}
 
