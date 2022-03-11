3e5a4d0213ad1715a94da3e56900f595115256f4
Prulm <jaredyared83@gmail.com>
2022-03-11 16:42:22 +0300

    added the source code for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - main block
+ * Description: Prints the whole alphabet in lowercase followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	return (0);
+}
