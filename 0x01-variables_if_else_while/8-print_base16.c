7b6a8f7237496af69704d0cbf139f9bed83f4f30
Prulm <jaredyared83@gmail.com>
2022-03-11 17:52:12 +0300

    added the source code for task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - main block
+ * Description: prints the alphabet in both cases
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	char ch = 'a';
+
+        while (n <= 9)
+	{
+		putchar(48 + n);
+		n++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
