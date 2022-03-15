82c5599db6a46dc6b2a16d8f55338e34854bea5d
Prulm <jaredyared83@gmail.com>
2022-03-15 18:34:46 +0300

    added the source code for task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ * prints the alphabet 10 times in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 9;
+
+	for (; j >= 0; j--)
+	{
+		print_alphabet(void);
+	}
+}
