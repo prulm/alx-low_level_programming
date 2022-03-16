ce013ebd7b4a9879073b345c78a01dd9701150a4
Prulm <jaredyared83@gmail.com>
2022-03-16 14:06:53 +0300

    added the source file for task 0 and the header file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Desc: Checks if positive_or_negative works
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
