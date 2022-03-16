62de19de4a680b72156ff2c1c72415e267971c22
Prulm <jaredyared83@gmail.com>
2022-03-16 15:19:37 +0300

    edited the source code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main - entry point
- * Description: Checks if 
- * positive_or_negative works
- * Return: 0 on success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
@@ -12,7 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
