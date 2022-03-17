7124ec49f137c486b54c2a6641e2209f9caf2711
Prulm <jaredyared83@gmail.com>
2022-03-17 10:03:13 +0300

    added the source file for task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter
+ * is uppercase
+ * Return: 1 if uppercase
+ * 0 otherwise
+ * @c - the letter
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
