2614110e45a048892842fdf8bc854440fec717b5
Prulm <jaredyared83@gmail.com>
2022-03-17 12:09:19 +0300

    added the source file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.c"
+
+/**
+ * more_numbers - prints numbers
+ * b/n 0 and 14 10 times
+ * @i - holds the numbers
+ * @j - character holder
+ * @c - holds characters
+ */
+
+void more_numbers(void)
+{
+	char j, c;
+
+	for (int i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			c = j;
+
+		if (j > 9)
+		{
+			_putchar('1');
+			c = j % 10;
+		}
+		_putchar('0' + c);
+		}
+		_putchar('\n');
+	}
+}
