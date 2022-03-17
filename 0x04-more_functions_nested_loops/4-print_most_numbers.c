fdc4d01459d02d15842f8fd576e492411ebd8f0f
Prulm <jaredyared83@gmail.com>
2022-03-17 17:58:32 +0300

    edited the source file for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_most_numbers -
  * prints all digits except 2 and 4
- * @i : holds the numbers
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
-	int i = 48;
+	int i;
 
-	for (; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		if (i == 50 || i == 52)	
 			continue;
