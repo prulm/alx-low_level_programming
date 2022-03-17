f392af9cb06382be9ffcabfb2cbdacaf28a63438
Prulm <jaredyared83@gmail.com>
2022-03-17 17:54:56 +0300

    edited the source file for task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * between 0 and 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+
+	}
+	_putchar('\n');
+}
