bdba95026d07fc9e6df4c1edeb9f38c8198c8179
Prulm <jaredyared83@gmail.com>
2022-03-17 19:21:11 +0300

    added the source file for task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - fizz buzz
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i > 1)
+			printf(" ");
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+printf("\n");
+}
