3dd6e948ba2c489c9f800582bd9e3632b09e68fe
Prulm <jaredyared83@gmail.com>
2022-03-21 16:35:22 +0300

    added the source file for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str : string
+ */
+
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	int j = i - 1;
+	while (str[j])
+	{
+		_putchar(str[j]);
+		j--;
+	}
+		_putchar('\n');
+}
+
