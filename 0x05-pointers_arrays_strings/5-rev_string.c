4039d8eaea584763366283d0b13289b8a256eba5
Prulm <jaredyared83@gmail.com>
2022-03-21 16:50:06 +0300

    edited the source file for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s : string to be rev
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	int j;
+
+	char *str;
+
+	*str = *s;
+
+	while (s[i])
+		i++;
+	j = i - 1;
+
+	i = 0;
+
+	while (s[j])
+		str[i] = s[j];
+}
