e197cefe0de058b9523b0df4727917c548b123e9
Prulm <jaredyared83@gmail.com>
2022-03-21 17:45:25 +0300

    edited the source file for task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,8 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	if (i % 2 != 0)
-		i = (i - 1) / 2;
-	else
-		i /= 2;
+	i++;
+	i /= 2;
 	while (str[i])
 	{
 		_putchar(str[i]);
