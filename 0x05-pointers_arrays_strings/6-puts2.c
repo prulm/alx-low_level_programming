497fc2479f8b9ffc57401327932b289b1fc95061
Prulm <jaredyared83@gmail.com>
2022-03-21 17:20:51 +0300

    added the source file for task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char
+ * @str : string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i])
+		i++;
+	}
+}
