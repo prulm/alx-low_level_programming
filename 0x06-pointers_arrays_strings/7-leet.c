546d2fa4847762252b8c323175d2f54d13d6fa91
Prulm <jaredyared83@gmail.com>
2022-03-24 13:49:12 +0300

    edited the source file for task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,17 +6,19 @@
  * Return: the encoded string
  */
 
-
 char *leet(char *s)
 {
 	int i, ii;
+
 	char s1[] = "aeotl";
+
 	char S1[] = "AEOTL";
+	
 	char s2[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-for (ii = 0; ii < 5; ii++)
+		for (ii = 0; ii < 5; ii++)
 		{
 			if (s[i] == s1[ii] || s[i] == S1[ii])
 			{
