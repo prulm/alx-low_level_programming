6e25cc4abc9be6936ef5bdeebd9dde3eafaf512f
Prulm <jaredyared83@gmail.com>
2022-03-24 12:06:20 +0300

    edited the source file for task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len,i;
+	int len, i;
 
-	for (len = 0; dest[len]!= '\0'; len++)
+	for (len = 0; dest[len] != '\0'; len++)
 	;
-	for (i = 0; src[i] != '\0'; i++,len++)
+	for (i = 0; src[i] != '\0'; i++, len++)
 	{
 		dest[len] = src[i];
 	}
