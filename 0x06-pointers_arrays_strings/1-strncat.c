dc845c3ef3f987267e7fd619d62e1d3b933a0615
Prulm <jaredyared83@gmail.com>
2022-03-24 12:27:58 +0300

    edited the source file for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (len = 0; dest[len] != '\0'; len++)
 	;
-	for (i = 0; i < n && src[i] != '\0'; i++, len++)
+	for (i = 0; (i < n) && (src[i] != '\0'); i++, len++)
 	{
 		dest[len] = src[i];
 	}
