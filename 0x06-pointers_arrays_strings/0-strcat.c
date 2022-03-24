c2c09962ac2fe0661f5c99d77982638eb5becc13
Prulm <jaredyared83@gmail.com>
2022-03-24 11:47:26 +0300

    added the source file for task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatinates strings
+ * @src : first string
+ * @dest : second string
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int len,i;
+
+	for (len = 0; dest[len]!= '\0';len++)
+	;
+	for (i = 0; src[i] != '\0'; i++,len++)
+	{
+		dest[len] = src[i];
+	}
+	printf("%s", s1);
+}
