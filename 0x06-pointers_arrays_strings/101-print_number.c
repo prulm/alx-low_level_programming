1c30fa149f3ebd3d233e106898245c0d43d017af
Prulm <jaredyared83@gmail.com>
2022-03-24 15:08:17 +0300

    edited the source file for task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,5 +7,26 @@
 
 void print_number(int n)
 {
+	if (n <= 9)
+	{
 	_putchar(48 + n);
+	}
+	else if (n <= 99)
+	{
+		_putchar(48 + (n / 10));
+		_putchar(48 + (n % 10));
+	}
+	else if (n <= 999)
+	{
+		_putchar(48 + (n / 100));
+		_putchar(48 + (((n % 100) - (n % 10)) / 10));
+		_putchar(48 + (n % 10);
+	}
+	else if (n <= 9999)
+	{
+		_putchar(48 + (n / 1000));
+		_putchar(48 + ((n % 1000) - (n % 100)) / 100);
+		_putchar(48 + (n % 100) / 10;
+		_putchar(48 + (n % 10);
+	}
 }
