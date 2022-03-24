48840c9fcf469ad549a56e4f0c25c6495043d8d1
Prulm <jaredyared83@gmail.com>
2022-03-24 15:11:30 +0300

    edited the source file for task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,29 @@
 #include "main.h"
 
 /**
- * print_number - prints int
- * @n : integer to be printed
+ * print_number - prints an integer.
+ * @n: integer
+ * Return: void
  */
 
 void print_number(int n)
 {
-	if (n <= 9)
-	{
-	_putchar(48 + n);
-	}
-	else if (n <= 99)
-	{
-		_putchar(48 + (n / 10));
-		_putchar(48 + (n % 10));
-	}
-	else if (n <= 999)
-	{
-		_putchar(48 + (n / 100));
-		_putchar(48 + (((n % 100) - (n % 10)) / 10));
-		_putchar(48 + (n % 10);
-	}
-	else if (n <= 9999)
-	{
-		_putchar(48 + (n / 1000));
-		_putchar(48 + ((n % 1000) - (n % 100)) / 100);
-		_putchar(48 + (n % 100) / 10;
-		_putchar(48 + (n % 10);
-	}
+int divisor = 1, i, resp;
+
+if (n < 0)
+{
+	_putchar('-');
+	n *= -1;
+}
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+	resp = n / divisor;
+	_putchar('0' + resp);
+
+}
+
 }
