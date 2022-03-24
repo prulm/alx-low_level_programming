094eb1218613cd7be9ddbe2bd08604de3d7cf213
Prulm <jaredyared83@gmail.com>
2022-03-24 13:50:25 +0300

    edited the source file for task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * leet - encodes a string
- * @s: string
- * Return: the encoded string
+ * leet - transform to leet
+ * @s: char array string type
+ * Return: s transformed
  */
 
 char *leet(char *s)
 {
 	int i, ii;
-
 	char s1[] = "aeotl";
-
 	char S1[] = "AEOTL";
-	
 	char s2[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
