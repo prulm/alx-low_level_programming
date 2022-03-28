d4c2d6eaf7d65d4983ada0b66d59807fd6488ad5
Prulm <jaredyared83@gmail.com>
2022-03-28 17:53:43 +0300

    added the source file for task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a char in a string
+ * @s - string
+ * @c - character
+ * Return: Null or character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s[i]);
+		else
+			return ('\0');
+	}
+}
