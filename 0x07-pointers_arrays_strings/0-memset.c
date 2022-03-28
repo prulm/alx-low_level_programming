dc37f4c2c96a2ce0130b89a59546d526c52b716e
Prulm <jaredyared83@gmail.com>
2022-03-28 17:17:46 +0300

    added the source file for task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with
+ * a constant byte
+ * @s : poiter to a string
+ * @b : character to be stored
+ * @n : number of mem locations
+ * Return: pointer to the memory area S
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
