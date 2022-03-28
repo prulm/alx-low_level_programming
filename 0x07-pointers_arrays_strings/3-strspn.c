07485b65e31d645d44e0b6853fbc8bae9a0949e1
Prulm <jaredyared83@gmail.com>
2022-03-28 18:45:54 +0300

    edited the source file for task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,10 +15,17 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (j = 0; s[j] >= '\0'; j++)
 	{
-		for (k = 0; accept >= '\0'; k++)
+		for (k = 0; accept > '\0'; k++)
 		{
-		if (s[j] == accept[k])
-			i++;
+			if (s[j] == accept[k])
+			{
+				i++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
 		}
 	}
 	return (i);
