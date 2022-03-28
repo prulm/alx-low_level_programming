6f713e83a11141433eac6907d105b1eab3203680
Prulm <jaredyared83@gmail.com>
2022-03-28 18:35:02 +0300

    edited the source file for task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of a prefix substring
+ * @s : string
+ * @accept : prefix substring
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j, k;
+
+	for (j = 0; s[j] >= '\0'; j++)
+	{
+		for (k = 0; accept >= '\0'; k++)
+		{
+		if (s[j] == accept[k])
+			i++;
+		}
+	}
+	return (i);
+}
