71e424a1e4c3cf125c39aa578e90085705167f39
Prulm <jaredyared83@gmail.com>
2022-03-28 17:30:45 +0300

    added the soure file for task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest : destination of mem area
+ * @src : source of memory area
+ * @n : number of mem areas copied
+ *
+ * Return : character
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
