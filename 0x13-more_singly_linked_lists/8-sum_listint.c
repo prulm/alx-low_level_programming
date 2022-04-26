2e865afe46371b39c187dead4329db2343d9bb11
Prulm <jaredyared83@gmail.com>
2022-04-26 21:45:21 +0300

    added the source file for task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - prints all the elements of a list.
+ * @head: head of a list.
+ * Return: numbers of nodes.
+ */
+int sum_listint(const listint_t *head)
+{
+	int sum = 0;
+	listint_t trav = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		sum = sum + trav->n;
+	}
+	return (sum);
+}
