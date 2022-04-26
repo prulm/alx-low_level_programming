92adc259295f172625d4a7aad33cf431f4386a08
Prulm <jaredyared83@gmail.com>
2022-04-26 21:50:04 +0300

    added the source file for task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,12 +3,17 @@
 /**
  * sum_listint - prints all the elements of a list.
  * @head: head of a list.
- * Return: numbers of nodes.
+ * Return: sum.
  */
 int sum_listint(const listint_t *head)
 {
 	int sum = 0;
-	listint_t *trav = *head;
+	listint_t *trav;
+
+	if (head == NULL)
+		return (0);
+
+	trav = head;
 
 	while (head != NULL)
 	{
