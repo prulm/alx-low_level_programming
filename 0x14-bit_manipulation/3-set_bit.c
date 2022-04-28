c95aee4b111c53df3b24550b606dfcc01937c3ae
Prulm <jaredyared83@gmail.com>
2022-04-28 14:43:28 +0300

    edited the source file for task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
+	if (index >= (sizeof(unsigned long int) * BYTE_LENGTH))
 		return (-1);
 
 	*n ^= (1 << index);
